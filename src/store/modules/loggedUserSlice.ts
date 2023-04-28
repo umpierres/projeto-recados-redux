3d4b7e1487de8882c67ab8120add3f985f557054
umpierres <alexandraumpierres@outlook.com>
2023-04-28 18:14:04 -0300

    trocando importação

--- src/store/modules/loggedUserSlice.ts
+++ src/store/modules/loggedUserSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import UserType from '../../types/UserType';
+import UserType from '../../types/userType';
 
 interface UserState {
     user: UserType;
