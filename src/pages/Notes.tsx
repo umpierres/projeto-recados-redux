5ed440703f48a8bd29162a7190f946549cc8c906
umpierres <alexandraumpierres@outlook.com>
2023-04-28 18:15:05 -0300

    adicionando adapter e slice para criar e mostrar tasks

--- src/pages/Notes.tsx
+++ src/pages/Notes.tsx
@@ -6,13 +6,22 @@ import {
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ModalCreate } from '../components/ModalCreate';
-import { useAppSelector } from '../store/hooks';
+import { useAppDispatch, useAppSelector } from '../store/hooks';
+import BasicCard from '../components/BasicCard';
+import { SelectAllTasks, addTask } from '../store/modules/tasksSlice';
 
 const Notes: React.FC = () => {
   const navigate = useNavigate();
   const [openModal, setOpenModal] = useState<boolean>(false);
   const rememberedLoggedUser = useAppSelector((state) => state.loggedUser.user);
   const loggedUser = sessionStorage.getItem('loggedUser');
+
+  const dispatch = useAppDispatch();
+
+  const allTasks = useAppSelector(SelectAllTasks);
+  const userLoggedTasks = allTasks.filter((task) => task?.owner === rememberedLoggedUser.email);
+  console.log(userLoggedTasks);
+  console.log(allTasks);
 
   useEffect(() => {
     if (rememberedLoggedUser.email === '') {
@@ -26,23 +35,56 @@ const Notes: React.FC = () => {
     setOpenModal(false);
   };
 
+  const handleConfirm = () => {
+    dispatch(
+      addTask({
+        id: Date.now(),
+        task: 'Roda roda',
+        detail: 'blablalblallbllal',
+        favorite: false,
+        time: 'Date.now()',
+        date: '23/02/32',
+        owner: 'e2u@gmail.com',
+      }),
+    );
+    setOpenModal(false);
+  };
+
   return (
     <>
-      <ModalCreate open={openModal} actionCancel={handleClose} description="Digite seu recado" title="Salvar recados" />
+      <ModalCreate
+        open={openModal}
+        actionCancel={handleClose}
+        actionConfirm={handleConfirm}
+        description="Digite o titulo e descrição do seu recado"
+        title="Crie o seu recado"
+      />
       <Grid container>
-        <Grid item xs={12}>
-          <Container sx={{ marginTop: '20px' }}>
+        <Grid item xs={12} m={5}>
+          <Container>
             <Typography variant="h4">Meus recados:</Typography>
             <Divider />
           </Container>
         </Grid>
+
+        {userLoggedTasks.map((task) => (
+          <Grid item xs={12} md={6} lg={3}>
+            <Container sx={{ marginTop: '20px' }}>
+              <BasicCard key={task?.id} title={task?.task} detail={task?.detail} time={task?.time} date={task?.date} />
+            </Container>
+          </Grid>
+        ))}
       </Grid>
-      <Fab color="primary" aria-label="add" size="small" sx={{ position: 'fixed', right: '20px', bottom: '20px' }}>
-        <AddIcon
-          onClick={() => {
-            setOpenModal(true);
-          }}
-        />
+      <Fab
+        color="primary"
+        aria-label="add"
+        size="small"
+        sx={{ position: 'fixed', right: '20px', bottom: '20px' }}
+        onClick={() => {
+          setOpenModal(true);
+        }}
+      >
+        <AddIcon />
       </Fab>
     </>
   );
