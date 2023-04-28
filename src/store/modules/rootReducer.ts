30c000aa1c9b8e98d0eb59a87dca8de4fc8fa8aa
umpierres <alexandraumpierres@outlook.com>
2023-04-28 18:13:49 -0300

    add tasksSlice

--- src/store/modules/rootReducer.ts
+++ src/store/modules/rootReducer.ts
@@ -2,8 +2,10 @@ import { combineReducers } from '@reduxjs/toolkit';
 
 import usersSlice from './usersSlice';
 import loggedUserSlice from './loggedUserSlice';
+import TasksSlice from './tasksSlice';
 
 export default combineReducers({
   users: usersSlice,
   loggedUser: loggedUserSlice,
+  tasks: TasksSlice,
 });
