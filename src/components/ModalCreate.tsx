a423fab52eeb22b00c9b82277ec4f18be8467641
umpierres <alexandraumpierres@outlook.com>
2023-04-30 14:49:33 -0300

    criando states e novos inputs

--- src/components/ModalCreate.tsx
+++ src/components/ModalCreate.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
@@ -6,8 +6,9 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import { useAppDispatch } from '../store/hooks';
+import { useAppDispatch, useAppSelector } from '../store/hooks';
 import { addTask } from '../store/modules/tasksSlice';
+import Task from '../types/taskType';
 
 interface ModalCreateProps {
   title: string;
@@ -22,32 +23,39 @@ interface ModalCreateProps {
 export const ModalCreate: React.FC<ModalCreateProps> = ({
   open, actionConfirm, actionCancel, title, description,
 }) => {
+  const [taskTitle, setTaskTitle] = useState<string>('aaaaa');
+  const [taskDescription, setTaskDescription] = useState<string>('aaaaaa');
+
+  const rememberedLoggedUser = useAppSelector((state) => state.loggedUser.user);
+
   const dispatch = useAppDispatch();
+
   const handleClose = () => {
     actionCancel();
   };
-
   const handleConfirm = () => {
-    dispatch(
-      addTask({
-        id: Date.now(),
-        task: 'Roda roda',
-        detail: 'blablalblallbllal',
-        favorite: false,
-        time: 'Date.now()',
-        date: '23/02/32',
-        owner: 'e2u@gmail.com',
-      }),
-    );
+    const data = new Date().toLocaleString();
+    const newTask: Task = {
+      id: Date.now(),
+      task: taskTitle,
+      detail: taskDescription,
+      favorite: false,
+      date: `${data}`,
+      owner: `${rememberedLoggedUser.email}`,
+    };
+    console.log(newTask.owner);
+    dispatch(addTask(newTask));
     actionConfirm();
   };
+
   return (
     <div>
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>{title}</DialogTitle>
         <DialogContent>
           <DialogContentText>{description}</DialogContentText>
-          <TextField autoFocus margin="dense" id="name" label="Email Address" type="email" fullWidth variant="standard" />
+          <TextField autoFocus margin="dense" id="title" label="Titulo do recado" type="text" fullWidth variant="standard" />
+          <TextField autoFocus margin="dense" id="detail" label="Descrição do recado" type="text" fullWidth variant="standard" />
         </DialogContent>
         <DialogActions>
           <Button sx={{ color: 'text.secondary' }} onClick={handleClose}>
