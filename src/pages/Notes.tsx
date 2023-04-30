b50d1f7dc745c3fabbe3f5faf7bc3a3cebf36f5a
umpierres <alexandraumpierres@outlook.com>
2023-04-30 17:37:42 -0300

    colocando o card para cá

--- src/pages/Notes.tsx
+++ src/pages/Notes.tsx
@@ -1,21 +1,33 @@
 import AddIcon from '@mui/icons-material/Add';
 
 import {
-  Container, Divider, Fab, Grid, Typography,
+  Container, Divider, Fab, Grid, Button, Card, CardActions, CardContent, Typography,
 } from '@mui/material';
 import React, { useEffect, useState } from 'react';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 import { useNavigate } from 'react-router-dom';
+import ModalDelete from '../components/ModalDelete';
 import { ModalCreate } from '../components/ModalCreate';
-import { useAppSelector } from '../store/hooks';
-import BasicCard from '../components/BasicCard';
-import { SelectAllTasks } from '../store/modules/tasksSlice';
+import { useAppDispatch, useAppSelector } from '../store/hooks';
+import { SelectAllTasks, removeTask } from '../store/modules/tasksSlice';
 
 const Notes: React.FC = () => {
   const navigate = useNavigate();
   const [openModal, setOpenModal] = useState<boolean>(false);
+  const [openModalDelete, setOpenModalDelete] = useState<boolean>(false);
   const rememberedLoggedUser = useAppSelector((state) => state.loggedUser.user);
 
   const allTasks = useAppSelector(SelectAllTasks);
+  const dispatch = useAppDispatch();
+
+  const handleDelete = (id: number) => {
+    dispatch(removeTask(id));
+    setOpenModalDelete(false);
+  };
+
   const userLoggedTasks = allTasks.filter((task) => task?.owner === rememberedLoggedUser.email);
 
   useEffect(() => {
@@ -26,6 +38,7 @@ const Notes: React.FC = () => {
 
   const handleClose = () => {
     setOpenModal(false);
+    setOpenModalDelete(false);
   };
 
   const actionConfirm = () => {
@@ -41,6 +54,7 @@ const Notes: React.FC = () => {
         description="Digite o titulo e descrição do seu recado"
         title="Crie o seu recado"
       />
+
       <Grid container>
         <Grid item xs={12} m={5}>
           <Container>
@@ -51,8 +65,37 @@ const Notes: React.FC = () => {
 
         {userLoggedTasks.map((task) => (
           <Grid item xs={12} md={6} lg={3} key={task?.id}>
+            <ModalDelete openModal={openModalDelete} actionCancel={handleClose} removeTask={handleDelete} id={task?.id} />
             <Container sx={{ marginTop: '20px' }}>
-              <BasicCard title={task?.task} detail={task?.detail} date={task?.date} />
+              <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                    {task.title}
+                  </Typography>
+                  <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    {task.date}
+                  </Typography>
+                  <Typography variant="body2" sx={{ wordBreak: 'break-all' }}>
+                    {task.detail}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">
+                    <FavoriteIcon sx={{ color: 'text.secondary' }} />
+                  </Button>
+                  <Button size="small">
+                    <EditIcon sx={{ color: 'text.secondary' }} />
+                  </Button>
+                  <Button
+                    size="small"
+                    onClick={() => {
+                      setOpenModalDelete(true);
+                    }}
+                  >
+                    <DeleteIcon sx={{ color: 'text.secondary' }} />
+                  </Button>
+                </CardActions>
+              </Card>
             </Container>
           </Grid>
         ))}
