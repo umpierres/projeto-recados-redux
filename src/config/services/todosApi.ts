55a1aafe45470f5ae8795bd0e562c88e1e704025
umpierres <alexandraumpierres@outlook.com>
2023-07-30 19:37:29 -0300

    arrumando para conseguir mexer na aplicação

--- src/config/services/todosApi.ts
+++ src/config/services/todosApi.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
-import 'dotenv/config';
 
 const serviceAPI = axios.create({
-  baseURL: `${process.env.REACT.APP_API_URL}`,
+  baseURL: 'http://localhost:8080',
 });
 
 export default serviceAPI;
