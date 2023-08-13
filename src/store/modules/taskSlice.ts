b02b4905de76a31c53c703e6f208a2e2745c8766
umpierres <alexandraumpierres@outlook.com>
2023-08-12 22:36:04 -0300

    adicionando slices de criaçao(falta add case ainda

--- src/store/modules/taskSlice.ts
+++ src/store/modules/taskSlice.ts
@@ -2,68 +2,28 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { AxiosError } from 'axios';
 import todosApi from '../../config/services/todosApi';
 import TaskType from '../../types/TaskType';
-import { RootState } from '..';
-import { ResponseCreateTask } from '../../types/ResponseCreateTask';
 import { hideAlert, showAlert } from './alertSlice';
-import { ResponseUpdateTask, UpdateNoteDTO } from '../../types/ResponseUpdateTask';
-import UserType from '../../types/UserType';
-import { ResponseGetTasks } from '../../types/ResponseGetTasks';
+import ResponseCreateTask from '../../types/ResponseCreateTask';
+import { ResponseTask } from '../../types/ResponseTask';
+import { OwnerAndFilter } from '../../types/SliceTypes';
 
 const initialState = {
   task: {
-    id: '',
+    noteID: '',
     ownerID: '',
+    notes: [] as TaskType[],
   },
   loading: false,
 };
 
 // create note
 
-export const createTask = createAsyncThunk('notes/create/', async (newTask: Omit<TaskType, 'id'>, { dispatch }) => {
+export const createTask = createAsyncThunk('notes/create', async (newTask: TaskType, { dispatch }) => {
   try {
     const response = await todosApi.post(`/notes/${newTask.ownerID}`, newTask);
+
     const dataAPI = response.data as ResponseCreateTask;
-    dispatch(
-      showAlert({
-        message: dataAPI.message,
-        type: 'success',
-        display: 'show',
-      }),
-    );
 
-    setTimeout(() => {
-      dispatch(hideAlert({ display: 'none', type: 'warning', message: '' }));
-    }, 1000);
-
-    return dataAPI;
-  } catch (error) {
-    if (error instanceof AxiosError) {
-      const dataAPI = error.response?.data as ResponseCreateTask;
-
-      dispatch(
-        showAlert({
-          message: dataAPI.message,
-          type: 'error',
-          display: 'show',
-        }),
-      );
-
-      setTimeout(() => {
-        dispatch(hideAlert({ display: 'none', type: 'warning', message: '' }));
-      }, 1000);
-
-      return dataAPI;
-    }
-    return { success: false, message: 'Algo está errado com essa aplicação! Chame o responsavel por ela' };
-  }
-});
-
-// update note
-
-export const updateTask = createAsyncThunk('/notes/edit/', async (updatedTask: UpdateNoteDTO, { dispatch }) => {
-  try {
-    const response = await todosApi.put(`/notes/${updatedTask.ownerID}/${updatedTask.id}`, updatedTask);
-    const dataAPI = response.data as ResponseUpdateTask;
     dispatch(
       showAlert({
         message: dataAPI.message,
@@ -100,15 +60,35 @@ export const updateTask = createAsyncThunk('/notes/edit/', async (updatedTask: U
   }
 });
 
-export const listTasks = createAsyncThunk('/notes/', async (ownerIDTask: string, { dispatch }) => {
+export const listTasks = createAsyncThunk('notes/list', async ({ ownerID, filter }: OwnerAndFilter, { dispatch }) => {
   try {
-    const response = await todosApi.get(`/notes/${ownerIDTask}`);
-    const dataAPI = response.data as ResponseGetTasks;
+    const response = await todosApi.get(`/notes/${ownerID}`, { params: { filter } });
+    const dataAPI = response.data as ResponseTask;
+    dispatch(
+      showAlert({
+        message: dataAPI.message,
+        type: 'success',
+        display: 'show',
+      }),
+    );
+    setTimeout(() => {
+      dispatch(hideAlert({ display: 'none', type: 'warning', message: '' }));
+    }, 1000);
     return dataAPI.data;
   } catch (error) {
     if (error instanceof AxiosError) {
-      const dataAPI = error.response?.data as ResponseGetTasks;
+      const dataAPI = error.response?.data as ResponseTask;
+      dispatch(
+        showAlert({
+          message: dataAPI.message,
+          type: 'error',
+          display: 'show',
+        }),
+      );
 
+      setTimeout(() => {
+        dispatch(hideAlert({ display: 'none', type: 'warning', message: '' }));
+      }, 1000);
       return dataAPI;
     }
 
@@ -116,10 +96,12 @@ export const listTasks = createAsyncThunk('/notes/', async (ownerIDTask: string,
   }
 });
 
-export const deleteTask = createAsyncThunk('/notes/delete/', async (noteId: string, { dispatch }) => {
+export const deleteTask = createAsyncThunk('notes/delete', async ({ ownerID, noteID }: { ownerID: string; noteID: string }, { dispatch }) => {
   try {
-    const response = await todosApi.post('/notes/delete/', noteId);
-    const dataAPI = response.data as ResponseUpdateTask;
+    const response = await todosApi.delete(`/notes/${ownerID}/${noteID}`);
+
+    const dataAPI = response.data as ResponseCreateTask;
+
     dispatch(
       showAlert({
         message: dataAPI.message,
@@ -152,35 +134,108 @@ export const deleteTask = createAsyncThunk('/notes/delete/', async (noteId: stri
       return dataAPI;
     }
 
-    return { success: false, message: 'Algo está errado com essa aplicação! Chame o responsavel por ela' };
+    return { success: false, message: 'Algo está errado com essa aplicação! Chame o responsável por ela' };
   }
 });
 
-export const toggleFavorite = createAsyncThunk('notes/toggle-favorite', async (noteId: string) => {
-  try {
-    const response = await todosApi.post(`/notes/update-note/${noteId}`, { favorite: true });
-    const dataAPI = response.data as ResponseUpdateTask;
-    return dataAPI;
-  } catch (error) {
-    return { success: false, message: 'Algo está errado com essa aplicação! Chame o responsável por ela.' };
-  }
-});
+export const updateTask = createAsyncThunk(
+  'notes/edit',
+  async ({ ownerID, noteID, updatedTask }: { ownerID: string; noteID: string; updatedTask: TaskType }, { dispatch }) => {
+    try {
+      const response = await todosApi.put(`/notes/${ownerID}/${noteID}`, updatedTask);
 
-export const toggleArchived = createAsyncThunk('notes/toggle-archived', async (noteId: string) => {
-  try {
-    const response = await todosApi.post(`/notes/update-note/${noteId}`, { archived: true });
-    const dataAPI = response.data as ResponseUpdateTask;
-    return dataAPI;
-  } catch (error) {
-    return { success: false, message: 'Algo está errado com essa aplicação! Chame o responsável por ela.' };
-  }
-});
+      const dataAPI = response.data as ResponseCreateTask;
+
+      dispatch(
+        showAlert({
+          message: dataAPI.message,
+          type: 'success',
+          display: 'show',
+        }),
+      );
+
+      setTimeout(() => {
+        dispatch(hideAlert({ display: 'none', type: 'warning', message: '' }));
+      }, 1000);
+
+      return dataAPI;
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        const dataAPI = error.response?.data as ResponseCreateTask;
+
+        dispatch(
+          showAlert({
+            message: dataAPI.message,
+            type: 'error',
+            display: 'show',
+          }),
+        );
+
+        setTimeout(() => {
+          dispatch(hideAlert({ display: 'none', type: 'warning', message: '' }));
+        }, 1000);
+
+        return dataAPI;
+      }
+
+      return { success: false, message: 'Algo está errado com essa aplicação! Chame o responsável por ela' };
+    }
+  },
+);
+
+export const toggleStatusTask = createAsyncThunk(
+  'notes/toggleStatus',
+  async ({ ownerID, noteID, action }: { ownerID: string; noteID: string; action: string }, { dispatch }) => {
+    try {
+      const response = await todosApi.put(`/notes/${ownerID}/${noteID}/${action}`);
+
+      const dataAPI = response.data as ResponseCreateTask;
+
+      dispatch(
+        showAlert({
+          message: dataAPI.message,
+          type: 'success',
+          display: 'show',
+        }),
+      );
+
+      setTimeout(() => {
+        dispatch(hideAlert({ display: 'none', type: 'warning', message: '' }));
+      }, 1000);
+
+      dispatch(listTasks({ ownerID, filter: {} }));
+
+      return dataAPI;
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        const dataAPI = error.response?.data as ResponseCreateTask;
+
+        dispatch(
+          showAlert({
+            message: dataAPI.message,
+            type: 'error',
+            display: 'show',
+          }),
+        );
+
+        setTimeout(() => {
+          dispatch(hideAlert({ display: 'none', type: 'warning', message: '' }));
+        }, 1000);
+
+        return dataAPI;
+      }
+
+      return { success: false, message: 'Algo está errado com essa aplicação! Chame o responsável por ela' };
+    }
+  },
+);
 
 export const TaskSlice = createSlice({
   name: 'task',
   initialState,
   reducers: {},
   extraReducers(builder) {
+    // criar
     builder.addCase(createTask.pending, (state) => {
       state.loading = true;
     });
@@ -190,7 +245,8 @@ export const TaskSlice = createSlice({
         return {
           ...state,
           task: {
-            id,
+            ...state.task,
+            noteID: id!,
             ownerID,
           },
           loading: false,
@@ -208,21 +264,22 @@ export const TaskSlice = createSlice({
       state.loading = false;
     });
 
-    builder.addCase(updateTask.pending, (state) => {
-      state.loading = true;
-    });
-    builder.addCase(updateTask.fulfilled, (state, action) => {
-      state.loading = false;
-    });
-    builder.addCase(updateTask.rejected, (state) => {
-      state.loading = false;
-    });
-
+    // listar
     builder.addCase(listTasks.pending, (state) => {
       state.loading = true;
     });
     builder.addCase(listTasks.fulfilled, (state, action) => {
-      state.loading = false;
+      if (action.payload && 'notes' in action.payload) {
+        return {
+          ...state,
+          task: {
+            ...state.task,
+            notes: action.payload.notes as TaskType[],
+          },
+          loading: false,
+        };
+      }
+      return state;
     });
     builder.addCase(listTasks.rejected, (state) => {
       state.loading = false;
@@ -232,30 +289,41 @@ export const TaskSlice = createSlice({
       state.loading = true;
     });
     builder.addCase(deleteTask.fulfilled, (state, action) => {
-      state.loading = false;
+      if (action.payload.success) {
+        return {
+          ...state,
+          loading: false,
+        };
+      }
+      return state;
     });
     builder.addCase(deleteTask.rejected, (state) => {
       state.loading = false;
     });
 
-    builder.addCase(toggleFavorite.pending, (state) => {
+    builder.addCase(updateTask.pending, (state) => {
       state.loading = true;
     });
-    builder.addCase(toggleFavorite.fulfilled, (state, action) => {
+    builder.addCase(updateTask.fulfilled, (state, action) => {
+      if (action.payload.success) {
+        return {
+          ...state,
+          loading: false,
+        };
+      }
+      return state;
+    });
+    builder.addCase(updateTask.rejected, (state) => {
       state.loading = false;
     });
-    builder.addCase(toggleFavorite.rejected, (state) => {
-      state.loading = false;
-    });
-
-    builder.addCase(toggleArchived.pending, (state) => {
-      state.loading = true;
-    });
-    builder.addCase(toggleArchived.fulfilled, (state, action) => {
-      state.loading = false;
-    });
-    builder.addCase(toggleArchived.rejected, (state) => {
-      state.loading = false;
+    builder.addCase(toggleStatusTask.fulfilled, (state, action) => {
+      if (action.payload.success) {
+        return {
+          ...state,
+          loading: false,
+        };
+      }
+      return state;
     });
   },
 });
